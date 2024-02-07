59b49d54df1b9d54986a2e419c9db1a166a3fa4b
Adilson <usadilsondigital@gmail.com>
2024-02-06 23:11:56 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ConsoleAppSqltest1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            String connectionString = "Data Source=DESKTOP-3H9KDN6\\SQLEXPRESS;Initial Catalog=janizeck;User ID=sa;Password=sa;Integrated Security=True;Asynchronous Processing=true;";
+           
+
+
+
+            Console.WriteLine("Please press any key to exit...");
+            Console.ReadKey();
+        }
+
+        //
+
+
+
+
+    }
+}
+
